dca826d44cd968b4fbb6e97c9a56a52717d332c0
Yi Xian <exian0716@gmail.com>
2024-10-09 19:51:29 +0800

    Complete `array-annotation`

---
+++ 3. array-annotation/1. array-type-bracket.ts
@@ -0,0 +1,20 @@
+// Bracket annotation
+const nums: number[] = [1, 2, 3, 4, 5];
+console.log(nums);
+
+const str: string[] = ["one", "two", "three"];
+console.log(str);
+
+// Append elements to array
+const items: string[] = [];
+items.push("Remote");
+items.push("Keyboard");
+// items.push(6)    // Error
+console.log(items)
+
+const numList: number[] = [];
+numList.push(20);
+numList.push(2000);
+numList.push(-1);
+// numList.push("Hello")    // Error
+console.log(numList);
---
+++ 3. array-annotation/3. multi-dimensional-array.ts
@@ -0,0 +1,7 @@
+const oneD: number[] = [1, 2, 3, 4, 5];
+const twoD: number[][] = [[1, 2, 3], [4, 5, 6]];
+const threeD: number[][][] = [[[1, 2, 3, 4, 5]]];
+
+console.log(oneD);
+console.log(twoD);
+console.log(threeD);
